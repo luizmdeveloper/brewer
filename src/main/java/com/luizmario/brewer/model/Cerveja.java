c924200d84cddfe6985cec54f8c263ba86dc5eca
luizmario <luizmariodev@gmail.com>
2017-09-26 15:23:38 -0300

    Ajustes mapeamento da cerveja

--- src/main/java/com/luizmario/brewer/model/Cerveja.java
+++ src/main/java/com/luizmario/brewer/model/Cerveja.java
@@ -11,11 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-@Entity(name="cerveja")
+@Entity
+@Table(name="cerveja")
 public class Cerveja {
 	
 	@Id
