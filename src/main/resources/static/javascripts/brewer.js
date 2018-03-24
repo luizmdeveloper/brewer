8f6f554687b6b8629581184e13bae3da6c0390c4
luizmario <luizmariodev@gmail.com>
2018-03-24 16:52:23 -0300

    Criando a máscara de telefone

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -15,7 +15,33 @@ Brewer.MaskMoney = (function(){
 	return MaskMoney;
 }());
 
+Brewer.PhoneNumberMask = (function(){
+	
+	function PhoneNumberMask (){
+		this.inputPhoneNumber = $('.js-phone-number')
+	}
+	
+	PhoneNumberMask.prototype.enable = function(){
+		var maskBehavior = function (val) {
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		}
+		var options = {
+				onKeyPress: function(val, e, field, options) {
+					field.mask(maskBehavior.apply({}, arguments), options);
+			    }
+		};
+
+		this.inputPhoneNumber.mask(maskBehavior, options);
+	}
+	
+	return PhoneNumberMask; 
+	
+})();
+
 $(function(){	
 	var maskMoney =  new Brewer.MaskMoney();
 	maskMoney.enable();	
+	
+	var phoneNumberMask = new Brewer.PhoneNumberMask();
+	phoneNumberMask.enable();
 });
