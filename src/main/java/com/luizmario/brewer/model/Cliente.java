0b70629d8245890aafa614e1ddd8d18a60a70af0
luizmario <luizmariodev@gmail.com>
2018-03-25 18:51:16 -0300

    Criando class modelo de cliente e sua migração

---
+++ src/main/java/com/luizmario/brewer/model/Cliente.java
@@ -0,0 +1,98 @@
+package com.luizmario.brewer.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	private String nome;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="tipo_pessoa")
+	private TipoPessoa tipoPessoa;
+	
+	@Column(name="cpf_cnpj")
+	private String cpfOuCnpj;
+	
+	private String telefone;
+	
+	private String email;
+	
+	@Embedded
+	private Endereco endereco;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+}
---
+++ src/main/resources/db/migration/V05__criar_tabela_cliente.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cliente (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(80) NOT NULL,
+	tipo_pessoa VARCHAR(15),
+	cpf_cnpj VARCHAR(30),
+	telfone VARCHAR(20),
+	email VARCHAR(50) NOT NULL,
+	logradouro VARCHAR(50),
+	complemento VARCHAR(30),
+	numero VARCHAR(10),
+	cep VARCHAR(15),
+	codigo_cidade BIGINT(20),
+	FOREIGN KEY (codigo_cidade) REFERENCES cidade(codigo)
+)ENGINE=InnoDB DEFAULT charset=utf8;
